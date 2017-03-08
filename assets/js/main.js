4ff291b20153251d51e0ec0951f00c9a37086b28
Karan More <karan.more3112@gmail.com>
2017-03-08 12:39:01 +0530

    added svg icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,33 @@
-// JavaScript Document
+$(function() {
+$('nav#menu').mmenu();
 
+
+
+$('.menu_hit').hover(function(){  
+     if($('#menu').hasClass('mmenu-opened'))
+      { 
+	 // $('#menu').addClass('mmenu-opened');  
+	     
+      }else{
+		 
+        $(this).trigger("click");
+       // $('.gloabl_serach_update .search_inn input[type=search]').focus();
+      }  
+}); 
+
+
+
+/*$('.call_search_update').click(function(){
+      //body-overlay
+      if($('body').hasClass('searchbox-open'))
+      {
+        $('body').removeClass('searchbox-open');
+      }else{
+        $('body').addClass('searchbox-open');
+        $('.gloabl_serach_update .search_inn input[type=search]').focus();
+      }
+      return false;
+    });
+});*/
+
+});
